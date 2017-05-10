3429d26707565bd30a2556eac525a46521ba1689
Eddie <380501005@qq.com>
2017-05-10 17:23:11 +0800

    newgit

---
+++ Swift笔记/Swift笔记/FourV/ViewControllers/FourMainVC.swift
@@ -0,0 +1,36 @@
+//
+//  FourMainVC.swift
+//  Swift笔记
+//
+//  Created by Eddie on 2017/5/10.
+//  Copyright © 2017年 Eddie. All rights reserved.
+//
+
+import UIKit
+
+class FourMainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        self.title = "更多"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swift笔记/Swift笔记/RootTabBarController.swift
@@ -0,0 +1,50 @@
+//
+//  RootTabBarController.swift
+//  Swift笔记
+//
+//  Created by Eddie on 2017/5/10.
+//  Copyright © 2017年 Eddie. All rights reserved.
+//
+
+import UIKit
+
+class RootTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initViewControllers();
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func initViewControllers(){
+
+        self.viewControllers = [initNav(imagestr: "mine", namestr: "书架", vC: MainVC()),initNav(imagestr: "category_gray", namestr: "分类", vC: TwoMainVC()),initNav(imagestr: "search_gray", namestr: "搜索", vC: ThreeMainVC()),initNav(imagestr: "more_gray", namestr: "更多", vC: FourMainVC())]
+    
+    }
+
+    
+    
+    private func initNav(imagestr : String , namestr : String , vC : UIViewController) -> UINavigationController
+    {
+        let image = UIImage.init(named: imagestr)
+        vC.tabBarItem = UITabBarItem.init(title: namestr, image: image, selectedImage: nil)
+        return  UINavigationController.init(rootViewController: vC)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
